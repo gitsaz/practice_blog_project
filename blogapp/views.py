85e5f29ac2a9d80a2231ff257e90f8b7269e2e10
Sazzadur Rahman <sazzadur570@gmail.com>
2026-03-24 00:35:59 +0600

    changed tags from static to dynamic

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from .models import Blog
+from .models import Blog, Category, Tag
 
 # Create your views here.
 def home(request):
     blogs = Blog.objects.all()
+    categories =Category.objects.all()
+    tags = Tag.objects.all()
     
     context = {
-        'blogs':blogs
+        'blogs':blogs,
+        'categories':categories,
+        'tags':tags
     }
     return render(request, 'home.html', context)
 
