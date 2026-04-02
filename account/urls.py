cc638504c74d59591bfcb58d7c18d55a5cf62b3c
Sazzadur Rahman <sazzadur570@gmail.com>
2026-04-02 23:28:33 +0600

    login and registration page ul

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from .views import(
+    login,
+    registration
+)
 urlpatterns = [
-    
+    path("login/", login, name="login"),
+    path("registration/", registration, name="registration")
 ]
